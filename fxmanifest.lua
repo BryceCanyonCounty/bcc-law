32d521ab66b1c4ceea7081fc14bdc4594b81a8ba
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-10-16 17:30:50 -0700

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+game 'rdr3'
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+lua54 'yes'
+author 'BCC Team'
+
+description 'A Complete Police Script'
+
+client_script {
+    'client/client.lua',
+    'client/functions.lua',
+    'client/menu.lua'
+
+}
+
+server_script {
+    'server/server.lua'
+}
+
+shared_script {
+    'locale.lua',
+    'languages/*.lua',
+    'config/ConfigMain.lua',
+    'config/ConfigWebhook.lua',
+    'config/ConfigJail.lua',
+    'config/ConfigCabinets.lua',
+    'config/ConfigService.lua'
+
+}
